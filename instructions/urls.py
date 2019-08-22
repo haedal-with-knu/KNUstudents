02d62dc65d55c1fd35dc2eaedca19823345b2273
LJBang <kailo123457@gmail.com>
2019-08-22 15:42:25 +0900

    add history

--- instructions/urls.py
+++ instructions/urls.py
@@ -7,5 +7,5 @@ app_name = 'instructions'
 urlpatterns=[
     path('', intro, name='intro'),
     path('depart/', depart, name='depart'),
-    path('history/', history, name='history'),
+    path('history/', History.as_view(), name='history'),
 ]
--- instructions/views.py
+++ instructions/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post, Photo
+from django.views.generic.list import ListView
 # Create your views here.
 def intro(request):
-  return render(request, 'instructions/intro.html')
+    return render(request, 'instructions/intro.html')
 
 def depart(request):
-  return render(request, 'instructions/depart.html')
+    return render(request, 'instructions/depart.html')
 
-def history(request):
-  return render(request, 'instructions/history.html')
+class History(ListView):
+    model = Post
+    template_name = 'instructions/history.html'
+    paginate_by = 6
