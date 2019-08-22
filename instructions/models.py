522a01d56fedac50c59f90681fa6227cbfb193af
LJBang <kailo123457@gmail.com>
2019-08-22 21:42:05 +0900

    history fix

--- instructions/models.py
+++ instructions/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_info_his', null=True, blank=True)
     postname = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='instructions/his/', default='null')
-    text = models.TextField()
-    updated = models.DateTimeField(auto_now=True)
-    class Meta:
-        ordering = ['-updated']
+    image = models.ImageField(upload_to='instructions/his_img/%Y/%m/%d', blank=True)
+    file = models.FileField(upload_to='instructions/his_file/%Y/%m/%d', blank=True)
+    text = models.TextField(blank=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.postname
