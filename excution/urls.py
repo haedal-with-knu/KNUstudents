b3767b6f73a2943a83d21e83061fe61f25ec47fd
Joo-ra-n <kjw995@gmail.com>
2019-09-26 17:31:18 +0900

    fuck

--- excution/urls.py
+++ excution/urls.py
@@ -7,12 +7,5 @@ app_name = 'excution'
 urlpatterns= [
     path('<str:category>/', category_all, name='category_all'),
     path('<str:category>/detail/<int:id>', post_detail, name='post_detail'),
-    path('introduction/nurse/', nurse, name='nurse'),
-    path('introduction/ksd/', ksd, name='ksd'),
-    path('introduction/engineering/', engineering, name='engineering'),
-    path('introduction/scitech/', scitech, name='scitech'),
-    path('introduction/teacher/', teacher, name='teacher'),
-    path('introduction/lifesci/', lifesci, name='lifesci'),
-    path('introduction/freemajor/', freemajor, name='freemajor'),
-    path('introduction/administration/', administration, name='administration'),
+
 ]
--- operation/views.py
+++ operation/views.py
@@ -24,3 +24,22 @@ def post_detail(request, category, id):
         cat = '단위별 소개'
         post = Post_introduction.objects.get(category=category, id=id)
     return render(request, 'operation/detail.html', {'post' : post, 'cat':cat})
+
+def nurse(request):
+    return render(request, 'operation/nurse.html')
+def ksd(request):
+    return render(request, 'operation/ksd.html')
+def engineering(request):
+    return render(request, 'operation/engineering.html')
+def scitech(request):
+    return render(request, 'operation/scitech.html')
+def teacher(request):
+    return render(request, 'operation/teacher.html')
+def lifesci(request):
+    return render(request, 'operation/lifesci.html')
+def freemajor(request):
+    return render(request, 'operation/freemajor.html')
+def administration(request):
+    return render(request, 'operation/administration.html')
+def sangju(request):
+    return render(request, 'operation/sangju.html')
