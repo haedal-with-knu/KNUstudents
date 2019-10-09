71998c42f0e24ad3b30693398a6da290811b7f9a
Joo-ra-n <kjw995@gmail.com>
2019-10-09 13:02:03 +0900

    added

--- excution/urls.py
+++ excution/urls.py
@@ -7,5 +7,12 @@ app_name = 'excution'
 urlpatterns= [
     path('<str:category>/', category_all, name='category_all'),
     path('<str:category>/detail/<int:id>', post_detail, name='post_detail'),
+    path('excution/#', 1, name='#'),
+    path('excution/#', 2, name='#'),
+    path('excution/#', 3, name='#'),
+    path('excution/#', 4, name='#'),
+    path('excution/#', 5, name='#'),
+    path('excution/#', 6, name='#'),
+    path('excution/#', 7, name='#'),
 
 ]
--- excution/views.py
+++ excution/views.py
@@ -22,5 +22,16 @@ def post_detail(request, category, id):
         post = Post_introduction.objects.get(category=category, id=id)
     return render(request, 'excution/detail.html', {'post' : post, 'cat':cat})
 
-
+def 1(request):
+    return render(request, 'excution/1.html')
+def 2(request):
+    return render(request, 'excution/2.html')
+def 3(request):
+    return render(request, 'excution/3.html')
+def 4(request):
+    return render(request, 'excution/4.html')
+def 5(request):
+    return render(request, 'excution/5.html')
+def 6(request):
+    return render(request, 'excution/6.html')
 
